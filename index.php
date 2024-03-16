5a4a6f1ae390dab8d2ef0446ea8af32acfbd0e14
motzzki <tizontimothy96@gmail.com>
2024-03-16 18:02:06 +0800

    first comm

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_index.css">
+    <title>Document</title>
+</head>
+<body>
+   <div class="main-container">
+        <header>
+            <h1>Bookshop</h1>
+            <div class="list">
+               <ul><a href="#">Home</a></ul>
+               <ul><a href="#">Contact</a></ul>
+               <ul><a href="#">About</a></ul>
+            </div>
+        </header>
+        <section>
+            <form action="">
+                <div>
+                    <label for="">Title</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Category</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Author</label>
+                    <input type="text">
+                </div>
+                <div>
+                    <label for="">Publisher</label>
+                    <input type="text">
+                </div>
+            </form>
+        </section>
+   </div>
+</body>
+</html>
